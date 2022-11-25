c1d53d08ac6a731c9bf57bceb648c233310d5fa1
Lysak-Anna <112387164+Lysak-Anna@users.noreply.github.com>
2022-11-25 16:36:33 +0000

    Deploying to gh-pages from @ Lysak-Anna/goit-js-hw-09nm@6488ec009db168211920032afea0e433af021432 🚀

---
+++ 01-color-switcher.8a5ee3a5.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let o=null;t.addEventListener("click",(function(e){o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.setAttribute("disabled","true")}),1e3)})),e.addEventListener("click",(function(e){clearInterval(o),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.8a5ee3a5.js.map
